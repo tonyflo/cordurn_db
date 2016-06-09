c6070072a9acb57e25221a2df646400529023b5f
Tony Florida <tflorida17@gmail.com>
2016-06-09 18:52:32 -0400

    #7: An attempt to sanatize earnings release data.

--- procedures/sanatize_earnings_release.sql
+++ procedures/sanatize_earnings_release.sql
@@ -3,22 +3,34 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS sanatize_earnings_release //
 
 /*
-Description: Update any garbase release time to 'Time Not Supplied'
-Example: CALL sanatize_earnings_release;
+Sanatize earnings release data
+
+Example:
+CALL sanatize_earnings_release;
 */
 CREATE PROCEDURE sanatize_earnings_release()
 BEGIN
 	SET SQL_SAFE_UPDATES = 0;
 
+	-- update garbase release time to 'Time Not Supplied'
 	UPDATE earnings_release dest,
 	(SELECT * FROM earnings_release
 	WHERE release_time != 'Before Market Open'
 		AND release_time != 'After Market Close' 
 		AND release_time != 'Time Not Supplied'
-		AND release_time NOT LIKE '% ET') src
+		AND release_time NOT LIKE '% ET'
+		AND release_time NOT LIKE '%:%:%') src
 	SET dest.release_time = 'Time Not Supplied'
 	WHERE src.earnings_id = dest.earnings_id;
 
+	-- update time from 12 hour 'HH:MM [ap]m ET' to 24 hour 'HH:MM:00'
+	UPDATE earnings_release dest,
+	(SELECT * FROM earnings_release
+	WHERE release_time LIKE '% ET') src
+	SET dest.release_time = TIME( STR_TO_DATE( src.release_time, '%h:%i %p' ) )
+	WHERE src.earnings_id = dest.earnings_id;
+
 	SET SQL_SAFE_UPDATES = 1;
+
 END //
 DELIMITER ;
